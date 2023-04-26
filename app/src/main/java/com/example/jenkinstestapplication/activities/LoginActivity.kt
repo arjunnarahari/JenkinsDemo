beb2fac8de6a939ec3f58715d798bf2a994d443b
LAPTOP-2QLEGOAU\narah <arjun.narahari@truemeds.in>
2023-04-26 15:52:41 +0530

    text changed

--- app/src/main/java/com/example/jenkinstestapplication/activities/LoginActivity.kt
+++ app/src/main/java/com/example/jenkinstestapplication/activities/LoginActivity.kt
@@ -25,7 +25,7 @@ class LoginActivity : AppCompatActivity() {
             startActivity(
                 Intent(context, HomeActivity::class.java)
                     .apply {
-                        putExtra(Constants.SOURCE, "from activity Login "+Constants.LOGIN_ACTIIVITY)
+                        putExtra(Constants.SOURCE, "from activity one "+Constants.LOGIN_ACTIIVITY)
                     })
         }
     }
